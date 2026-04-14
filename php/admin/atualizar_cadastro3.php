a9d0f6bb5d91fd97b0b40e58ba2b72b502c20d57
pclevy <pclevybr@gmail.com>
2026-04-14 13:40:54 -0300

    'cadastro_atualizar3.php' teste3 - 14/04/2026, 13:40

--- php/admin/atualizar_cadastro3.php
+++ php/admin/atualizar_cadastro3.php
@@ -24,9 +24,9 @@
 	$sqltabs=pg_query($conexao,"SELECT nome_tab FROM tabelas_rating ORDER BY nome_tab") or die("Sem Tabelas!");
 	$resultabs=pg_num_rows($sqltabs);
 	
-	echo "Qt. Tabelas: $resultabs <br><br>";exit;
+	echo "Qt. Tabelas: $resultabs <br><br>";//exit;
 	$resultabs=1; // *** teste *** 13:25 ***
-	echo "Qt. Tabelas: $resultabs <br><br>";exit;	
+	echo "Qt. Tabelas: $resultabs <br><br>";//exit;	
 	
 	for($i=0;$i<$resultabs;$i++)
 	{
