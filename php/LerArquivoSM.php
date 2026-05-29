40d4e2d67fafea5d93b0b773e809f75f3e96e62c
pclevy <pclevybr@gmail.com>
2026-05-29 00:30:20 -0300

    LerArquivoSM.php, correção - 29/05/2026, 00:30

--- php/LerArquivoSM.php
+++ php/LerArquivoSM.php
@@ -1452,6 +1452,9 @@
 													
 													$diffrat = ($RatFIDE[$CodJogA]-$RatFIDE[$CodJogB]);
 													if($diffrat>400){$diffrat=400;}elseif($diffrat<-400){$diffrat=-400;};
+													
+													echo "$diffrat";exit;
+													
 													$ratvar[$CodJogA][$r] = $diffrat > 0 ? -(($expectancia[abs($diffrat)])*$FatorK[$CodJogA])/100:-((100-$expectancia[abs($diffrat)])*$FatorK[$CodJogA])/100;
 													$ratvar[$CodJogB][$r] = $diffrat < 0 ? ((100-$expectancia[abs($diffrat)])*$FatorK[$CodJogB])/100:(($expectancia[abs($diffrat)])*$FatorK[$CodJogB])/100;
 													$ratvart[$CodJogA] = $ratvart[$CodJogA] + $ratvar[$CodJogA][$r];
