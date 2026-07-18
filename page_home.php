6d3060447ab2de23ae7daf234eb7ce10a4a202ed
pclevy <pclevybr@gmail.com>
2026-07-17 22:59:40 -0300

    'page_home.php' - 2026/07/17, 22:59

--- page_home.php
+++ page_home.php
@@ -13,9 +13,8 @@
 			A responsabilidade por idealização, construção e execução das propostas acima, será regida no mais alto grau dos seguintes critérios:<br>
 			<center>"<b><u>Legalidade</u> - <u>Transparência</u> - <u>Inclusão</u> - <u>Respeito</u> - <u>Desenvolvimento</u> - <u>Modernidade</u></b>".</center>
 			<font size="1"><br></font>
+			<div  style="float:right;font-size:18px;"><b>AI Paulo C. Levy</u></b></div>
 		</div>
-
-		<div  style="float:right;font-size:18px;"><b><u>AI Paulo C. Levy</u></b></div>
 
 		<!br>
 		<div style="visibility:visible;width:100%;height:80px;">	
