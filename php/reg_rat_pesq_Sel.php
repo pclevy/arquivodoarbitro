119d8e6f23d7451cd0559b06c9e84be96ff76915
pclevy <pclevybr@gmail.com>
2026-09-01 02:05:07 -0300

    'reg_rat_pesq_Sel.php' - 2026/09/01, 02:05

--- php/reg_rat_pesq_Sel.php
+++ php/reg_rat_pesq_Sel.php
@@ -1,5 +1,5 @@
 <?php
-/* php/reg_rat_pesq_Sel.php - Versão: 2026.8.11 - Alterado em 2026/08/11, 22:50 */
+/* php/reg_rat_pesq_Sel.php - Versão: 2026.09.01 - Alterado em 2026/09/01, 02:03 */
 
 	ini_set('display_errors', 0);
 	ini_set('display_startup_errors', 0);
@@ -31,7 +31,6 @@
 			dt_nasc,
 			right(trim(dt_nasc), 4) AS ano_nasc
 		FROM cadastro
-		
 		ORDER BY nome;");
 	  
 	$resultado = pg_num_rows($sql);
@@ -40,11 +39,14 @@
 	echo "<script language='JavaScript' type='text/javascript'>";
 	echo "var listaJogadores = [];";
 	while ($i<$resultado) {
+		
 		$reg = trim(" ".pg_fetch_result($sql,$i,'reg'));
+		
 		//$prenome = trim(" ".pg_fetch_result($sql,$i,'nome'));
 		//$sobrenome = trim(" ".pg_fetch_result($sql,$i,'sobrenome'));
 		//$nome=trim($prenome . ' ' . $sobrenome);
 		$nome = pg_fetch_result($sql,$i,'nomecompleto');
+		
 		//$clube = trim(" ".pg_fetch_result($sql,$i,'clube'));
 		$clube = pg_fetch_result($sql,$i,'clube');
 
@@ -52,7 +54,8 @@
 		
 		//echo "$reg";
 		//echo "console.log('$reg - $nome - $clube');";
-		echo "console.log(" . json_encode("$reg - $nome - $clube") . ");";		
+		
+		//echo "console.log(" . json_encode("$reg - $nome - $clube") . ");";		
 		
 		$i++;
 	}
@@ -187,56 +190,66 @@
 	
 	<body bgcolor="eeeeff">
 		<!-- Título: <input name='titulo' id='titulo' type='text' value='' size='6' maxlenght='6'> -->
+		
+		<!--
 		<font size="3"><b>Xadrez UERJ</b></font><br>
 		<font size="6">Arquivo do Árbitro</font><br>
-		<font size="3"><b>Pesquisa de Enxadristas - por nome</b></font> <font size='2' color='red'>(Lista de Rating de julho/2026!)</font><br>
-		<font size='2' color='red'> <!-- <b>Em construção</b>: os dados aqui apresentados ainda são experimentais, podendo haver imprecisões!!</font> --> <br>
-		
-		<!-- <input name='titulo' id='titulo' type='text' value='' size='6' maxlenght='6'> -->
-		<form name='reg_rat_pesq' action='reg_rat_pesq.php' method='post' autocomplete='off'>
-			<div style="width:460;background-color:#EDFAD6;line-height:30px;padding:1;border:1px solid #2266AA;">
-				<table width='100%'>
-					<tr>
-						<td valign='top' colspan='4'>
-							Clube: <input name='clube' id='clube' type='text' value='' size='10' maxlenght='10'>
-							&nbsp; &nbsp; 
-							&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-							Status: 
-							<select name='status' id='status'>
-								<option value='N'>Filiados</option>
-								<option value='S' selected='true'>Todos</option>
-							</select>
-						</td>
-					</tr>
-					
-					<tr>
-						<td valign='top' colspan='4'>Faixa de Rating.&nbsp;De: 
-							<input name='rat_min' id='rat_min' type='text' value='0' size='2' maxlenght='4'>&nbsp;A&nbsp;
-							<input name='rat_max' id='rat_max' type='text' value='3000' size='2' maxlenght='4'>
-							&nbsp; &nbsp; &nbsp; 
-							Ritmo: 
-							<select name='ritmo' id='ritmo'>
-								<option value='S' selected='true'>Clássico</option>
-								<option value='Q'>Rápido</option>
-								<option value='B'>Relâmpago</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td valign='top'>Nome:</td>
-						<td colspan='4'>
-							<input name='enxadrista' id='enxadrista' type='text' value='' size='49' maxlenght='60' onkeyup='TamNomPesq=trim(this.value).length;pesq_nome(this.value);' />
-							<input name='enxadrista_reg' id='enxadrista_reg' type='hidden' value='' size='6' />
-							
-							<select name='enxadrista_list' id='enxadrista_list' size='15' style="width:340px" onchange='Select_Click(this,"clk");' ondblclick='Select_Click(this,"dbl");'>
-							
-								<option style="font-weight:bold" value="">Digite parte do nome ou Selecione abaixo ...</option>
-							</select>
-						</td>
-					</tr>
-					<tr><td>&nbsp;</td><td><input id='SubmitButton' type='submit' onClick="if(enxadrista_reg.value<1 && clube.value=='' && titulo.value=='' && rat_min.value=='' && rat_max.value==''){alert('Clique em um nome da Lista e/ou escolha um outro critério!!');enxadrista_list.focus(); return false;}" name='Enviar' value='Enviar'></td></tr>
-				</table>
+		-->
+		<center>
+			<font size="5"><b>Projeto Esfinge</b></font><br>
+			<font size="6"><b>Xadrez de Olho no Futuro</b></font><br>
+			<font size="2">(Antigo<b> Xadrez UERJ</b>)</font><br>
+			<font size="6">Arquivo do Árbitro</font><br>
+			
+			<font size="3"><b>Pesquisa de Enxadristas - por nome</b></font> <font size='2' color='red'>(Lista de Rating de julho/2026!)</font><br>
+			<font size='2' color='red'> <!-- <b>Em construção</b>: os dados aqui apresentados ainda são experimentais, podendo haver imprecisões!!</font> --> <br>
+			
+			<div style="max-width:460px;background-color:#EDFAD6;line-height:30px; border:1px solid #2266AA;">
+				<!-- <input name='titulo' id='titulo' type='text' value='' size='6' maxlenght='6'> -->
+				<form name='reg_rat_pesq' action='reg_rat_pesq.php' method='post' autocomplete='off'>
+					<table width='100%'>
+						<tr>
+							<td valign='top' colspan='4'>
+								Clube: <input name='clube' id='clube' type='text' value='' size='10' maxlenght='10'>
+								&nbsp; &nbsp; 
+								&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+								Status: 
+								<select name='status' id='status'>
+									<option value='N'>Filiados</option>
+									<option value='S' selected='true'>Todos</option>
+								</select>
+							</td>
+						</tr>
+						
+						<tr>
+							<td valign='top' colspan='4'>Faixa de Rating.&nbsp;De: 
+								<input name='rat_min' id='rat_min' type='text' value='0' size='2' maxlenght='4'>&nbsp;A&nbsp;
+								<input name='rat_max' id='rat_max' type='text' value='3000' size='2' maxlenght='4'>
+								&nbsp; &nbsp; &nbsp; 
+								Ritmo: 
+								<select name='ritmo' id='ritmo'>
+									<option value='S' selected='true'>Clássico</option>
+									<option value='Q'>Rápido</option>
+									<option value='B'>Relâmpago</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td valign='top'>Nome:</td>
+							<td colspan='4'>
+								<input name='enxadrista' id='enxadrista' type='text' value='' size='49' maxlenght='60' onkeyup='TamNomPesq=trim(this.value).length;pesq_nome(this.value);' />
+								<input name='enxadrista_reg' id='enxadrista_reg' type='hidden' value='' size='6' />
+								
+								<select name='enxadrista_list' id='enxadrista_list' size='15' style="width:340px" onchange='Select_Click(this,"clk");' ondblclick='Select_Click(this,"dbl");'>
+								
+									<option style="font-weight:bold" value="">Digite parte do nome ou Selecione abaixo ...</option>
+								</select>
+							</td>
+						</tr>
+						<tr><td>&nbsp;</td><td><input id='SubmitButton' type='submit' onClick="if(enxadrista_reg.value<1 && clube.value=='' && titulo.value=='' && rat_min.value=='' && rat_max.value==''){alert('Clique em um nome da Lista e/ou escolha um outro critério!!');enxadrista_list.focus(); return false;}" name='Enviar' value='Enviar'></td></tr>
+					</table>
+				</form>
 			</div>
-		</form>
+		</center>
 	</body>
 </html>
