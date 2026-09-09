505b9ab672f410cdc57c6d86529120afe67608d5
pclevy <pclevybr@gmail.com>
2026-09-09 02:38:23 -0300

    'page_home.php, php/index.php' - 2026/09/09, 02:38

--- php/index.php
+++ php/index.php
@@ -119,6 +119,7 @@
 
 
 				<!-- Pesquisar Torneios -->
+
 				<span
 					title="Pesquisar Torneios"
 					style="color:#999; cursor:not-allowed;"
@@ -126,9 +127,11 @@
 					Pesquisar Torneios
 				</span><br>
 
-				<span style="font-size:6px;">yyyyy<br></span>
+				<span style="font-size:6px;"><br></span>
+
 
 				<!-- Enviar Torneios SM -->
+
 				<span
 					title="Torneios Swiss Manager"
 					style="color:#999; cursor:not-allowed;"
@@ -136,7 +139,9 @@
 					Enviar Torneios SM
 				</span><br>
 
+
 				<!-- Enviar Torneios SP -->
+
 				<span
 					title="Torneios Swiss Perfect"
 					style="color:#999; cursor:not-allowed;"
@@ -144,7 +149,9 @@
 					Enviar Torneios SP
 				</span><br>
 
+
 				<!-- Enviar Torneios VG -->
+
 				<span
 					title="Torneios Vega"
 					style="color:#999; cursor:not-allowed;"
@@ -152,17 +159,23 @@
 					Enviar Torneios VG
 				</span><br>
 
+
 				<span style="font-size:6px;"><br></span>
+
+
 				<!-- Rating -->
+
 				<a href="index.php?page=RatingFexerj">
 					Rating Fexerj
 				</a>
 
 				<hr>
 
+
 				<!-- =================================================
 				     EVENTOS PRÓXIMOS
 				     ================================================= -->
+
 				<span style="font-size:16px;">
 					Eventos próximos:
 				</span>
