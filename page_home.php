2e2cd2caa0536337d6e1c90466c8b05dd0d911a1
pclevy <pclevybr@gmail.com>
2026-09-10 16:52:23 -0300

    adiciona erros

--- page_home.php
+++ page_home.php
@@ -11,6 +11,7 @@
 			Visando o engrandecimento de nosso esporte, devemos também proporcionar os instrumentos acima citados, aos enxadristas 
 			interessados em conhecer melhor as Regras de Competição que regem o Xadrez como Esporte.<br>
 			A responsabilidade por idealização, construção e execução das propostas acima, será regida no mais alto grau dos seguintes critérios:<br>
+			<br />
 			<center>"<b><u>Legalidade</u> - <u>Transparência</u> - <u>Inclusão</u> - <u>Respeito</u> - <u>Desenvolvimento</u> - <u>Modernidade</u></b>".</center>
 			<font size="1"><br></font>
 			<span style="float:right;font-size:18px;"><b><u>AI Paulo C. Levy</u></b></span><br>
